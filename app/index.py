cc41bec4336996c28b81801e800a54f66b7c2001
Fat1512 <letanphat15122004@gmail.com>
2024-11-18 07:50:46 +0700

    90% of homepage

--- app/index.py
+++ app/index.py
@@ -2,10 +2,10 @@ from flask import render_template
 import json
 from app import app
 
+
 @app.route("/")
 def index():
-
-    with open('data/category.json') as f:
+    with open('data/category.json', encoding="utf8") as f:
         data = json.load(f)
         category_section = data[4:8]
     advertised_category_image = [
@@ -14,31 +14,34 @@ def index():
         'https://res.cloudinary.com/dq27ted4k/image/upload/v1731746969/mtfd7avuzgrpuotg7aej.webp',
         'https://res.cloudinary.com/dq27ted4k/image/upload/v1731746969/rw19jx9s8295npm171bm.webp'
     ]
-
-    with open('data/new_release.json') as f:
-        data = json.load(f)
-        new_release = data[0:8]
-
-    bestselling_books = {}
-
-
     idx = 0
     for category in category_section:
         category['advertised_image'] = advertised_category_image[idx]
         idx += 1
+
+
+    with open('data/new_release.json', encoding="utf8") as f:
+        data = json.load(f)
+        new_release = data[0:8]
+
+    with open('data/bestselling_book.json', encoding="utf8") as f:
+        data = json.load(f)
+        bestselling_books = data[0:5]
 
     return render_template("home.html",
                            bestselling_books=bestselling_books,
                            new_release=new_release,
                            category_section=category_section)
 
+
 @app.route("/category")
 def get_category():
-    with open('data/category.json') as f:
+    with open('data/category.json', encoding="utf8") as f:
         data = json.load(f)
         categories = data[0:4]
 
     return categories
+
 
 if __name__ == "__main__":
     app.run(debug=True)
