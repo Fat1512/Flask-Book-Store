0f00d7fcf924707d2dcfa2e2cbb40d8d0020dbad
Fat1512 <letanphat15122004@gmail.com>
2024-12-23 16:31:03 +0700

    cm

--- app/controllers/CartController.py
+++ app/controllers/CartController.py
@@ -24,6 +24,7 @@ def checkout():
         if cart_ss is None:
             cart_ss = []
 
+
         # Update the cart session with new data
         cart_ss = [{
             'bookId': int(item['bookId']),
